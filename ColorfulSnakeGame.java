37aeaddeac4a473d6e9ea5686f5c8ffc6629600e
shasin theekshana <shasintheekshana2001@gmail.com>
2025-01-24 00:27:07 +0530

    Initialize game panel basics

---
+++ ColorfulSnakeGame.java
@@ -0,0 +1,39 @@
+package ColorfulSnakeGame;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ColorfulSnakeGame extends JFrame {
+    public ColorfulSnakeGame() {
+        add(new GamePanel());
+        setTitle("Colorful Snake Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(ColorfulSnakeGame::new);
+    }
+}
+
+class GamePanel extends JPanel {
+    private final int TILE_SIZE = 30;
+    private final int GAME_WIDTH = 600;
+    private final int GAME_HEIGHT = 600;
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
+        setBackground(Color.BLACK);
+        setFocusable(true);
+    }
+}
+
